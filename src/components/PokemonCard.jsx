c1ca1bed20e9378e914d7f92474401a4c7a5762d
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-03-26 16:15:09 +0100

    Rendre composant PokemonCard dynamique

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,34 @@
+
+const pokemonList = [
+    {
+        name: "bulbasaurE",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
-    return (<figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+
+    const pokemon = pokemonList[1];
+
+    // Etape 3 de la Quête 3 :
+
+    // return (<figure>
+    //     <img src={pokemon.imgSrc} />
+    //     <figcaption>{pokemon.name}</figcaption>
+    // </figure>
+    // );
+
+    //Etape 4 Quête 3 : 
+    return (
+        <figure>
+            {pokemon.imgSrc === undefined ? <div> <p> ???</p><figcaption>{pokemon.name}  </figcaption></div> : <div><img src={pokemon.imgSrc} /> <figcaption>{pokemon.name}</figcaption></div>}
+        </figure >
     );
 }
 
 export default PokemonCard;
+
