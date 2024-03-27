25f9cf665e87c7081dea16271f1edad87b604173
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-03-27 18:59:34 +0100

    Add state and event listener with button in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
 
   const pokemonList = [
     {
-      name: "bulbasaurE",
+      name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "Mew",
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
     },
   ];
 
+
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const handleClickSuivant = () => {
+    setpokemonIndex(pokemonIndex + 1);
+  }
+
+  const handleClickPrecedent = () => {
+    setpokemonIndex(pokemonIndex - 1);
+  }
+
+
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? <button onClick={handleClickPrecedent}>Précédent</button> : undefined}
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickSuivant}>Suivant</button> : undefined}
     </div>
   );
 }
